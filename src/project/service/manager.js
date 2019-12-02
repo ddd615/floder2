059f3ab242282c78c451c387a2b21b2a1fa2b075
ddd615 <670232662@qq.com>
2019-12-02 20:01:41 +0800

    11.21

---
+++ src/project/service/manager.js
@@ -0,0 +1,63 @@
+import axios from '../http/axios'
+const model = 'manager';
+let getUrl = `api/${model}/get`;
+let searchUrl = `api/${model}/search`;
+let countUrl = `api/${model}/count`;
+let deleteUrl = `api/${model}/delete`;
+let batchDeleteUrl = `api/${model}/delete`;
+let enableUrl = `api/${model}/enable`;
+let disableUrl = `api/${model}/disable`;
+
+export function get(param, callback) {
+  axios.post(getUrl, param).then(data => {
+    if (data !== undefined && data !== '' && data !== null) {
+      // callback when data is exist
+      callback(data)
+    }
+  })
+}
+
+export function search(param, callback) {
+  axios.post(searchUrl, param).then(data => {
+    if (data !== undefined && data !== '' && data !== null) {
+      // callback when data is exist
+      callback(data)
+    }
+  })
+}
+
+export function count(param, callback) {
+  axios.post(countUrl, param).then(data => {
+    if (data !== undefined && data !== '' && data !== null) {
+      // callback when data is exist
+      callback(data)
+    }
+  })
+}
+
+export function  del(param, callback) {
+  axios.post(countUrl, param).then(data => {
+    if (data !== undefined && data !== '' && data !== null) {
+      // callback when data is exist
+      callback(data)
+    }
+  })
+}
+
+export function enable(param, callback) {
+  axios.post(enableUrl, param).then(data => {
+    if (data !== undefined && data !== '' && data !== null) {
+      // callback when data is exist
+      callback(data)
+    }
+  })
+}
+
+export function disable(param, callback) {
+  axios.post(disableUrl, param).then(data => {
+    if (data !== undefined && data !== '' && data !== null) {
+      // callback when data is exist
+      callback(data)
+    }
+  })
+}
